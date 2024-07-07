af0ceccf526d4983d85dd4fee1868d47b5d586e9
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-07-07 11:13:11 +0900

    project settings

---
+++ src/main/java/com/sideprj/ipo/IpoApplication.java
@@ -0,0 +1,13 @@
+package com.sideprj.ipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpoApplication.class, args);
+    }
+
+}
