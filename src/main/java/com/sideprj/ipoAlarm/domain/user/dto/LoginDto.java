2790f6c3d3a33c88b41065b7b110b7a5f7a20c13
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-08-19 16:00:46 +0900

    swagger security 추가, 카카오 로그인 시 redis에 저장되는 데이터 형식 변경

--- src/main/java/com/sideprj/ipoAlarm/domain/user/dto/LoginDto.java
+++ src/main/java/com/sideprj/ipoAlarm/domain/user/dto/LoginDto.java
@@ -1,5 +1,8 @@
 package com.sideprj.ipoAlarm.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 @Getter
@@ -7,6 +10,13 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class LoginDto {
+
+    @Email
+    @NotEmpty(message = "Email cannot be null or empty")
+    @Schema(description = "Email is used for login", example = "test@test.com")
     private String email;
+
+    @NotEmpty(message = "Password cannot be null or empty")
+    @Schema(description = "User's password", example = "testtest")
     private String password;
 }
