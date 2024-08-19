531613f40005eaae37cedb8ff5335453c71192ad
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-08-19 13:44:14 +0900

    controller 에 swagger operation 추가

--- src/main/java/com/sideprj/ipoAlarm/domain/alarm/controller/AlarmController.java
+++ src/main/java/com/sideprj/ipoAlarm/domain/alarm/controller/AlarmController.java
@@ -3,10 +3,16 @@ package com.sideprj.ipoAlarm.domain.alarm.controller;
 import com.sideprj.ipoAlarm.domain.alarm.constants.AlarmConstants;
 import com.sideprj.ipoAlarm.domain.alarm.service.AlarmService;
 import com.sideprj.ipoAlarm.domain.alarm.vo.AlarmResponseVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +25,32 @@ public class AlarmController {
 
     private final AlarmService alarmService;
 
+    @Operation(
+            summary = "Save Alarm Rest API",
+            description = "Save Subscription Request"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP STATUS Created"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "HTTP STATUS UNAUTHORIZED"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP STATUS NOT FOUND"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+    }
+    )
     @PostMapping( value = "/data")
     public ResponseEntity<AlarmResponseVo> saveAlarm(@RequestParam String ipoName, Authentication authentication) {
         alarmService.save(ipoName,authentication);
--- src/main/java/com/sideprj/ipoAlarm/domain/mypage/controller/MyPageController.java
+++ src/main/java/com/sideprj/ipoAlarm/domain/mypage/controller/MyPageController.java
@@ -4,10 +4,16 @@ import com.sideprj.ipoAlarm.domain.mypage.MyAlarmDto;
 import com.sideprj.ipoAlarm.domain.mypage.MyPageDto;
 import com.sideprj.ipoAlarm.domain.mypage.service.MyPageService;
 import feign.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +27,32 @@ import java.util.List;
 public class MyPageController {
 
     private final MyPageService myPageService;
-
+    @Operation(
+            summary = "get userInfo Rest API",
+            description = "Save Subscription Request"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP STATUS OK"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "HTTP STATUS UNAUTHORIZED"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP STATUS NOT FOUND"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+    }
+    )
     @GetMapping("/data")
     public ResponseEntity<MyPageDto> getUserInfo() {
         return ResponseEntity
@@ -29,6 +60,32 @@ public class MyPageController {
                 .body(myPageService.userInfo());
     }
 
+    @Operation(
+            summary = "fetch my Alarm List Rest API",
+            description = "Save Subscription Request"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP STATUS OK"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "HTTP STATUS UNAUTHORIZED"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP STATUS NOT FOUND"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+    }
+    )
     @GetMapping("/myalarm")
     public ResponseEntity<List<MyAlarmDto>> getMyAlarm() {
         return ResponseEntity
