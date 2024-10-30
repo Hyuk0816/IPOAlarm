2e33a2e3efb7da3e8dcbc4856174c1937a50c0be
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-10-30 17:01:54 +0900

    ArgumentResolver @Parameter(hidden = true) 적용

--- src/main/java/com/sideprj/ipoAlarm/domain/user/util/UserInfo.java
+++ src/main/java/com/sideprj/ipoAlarm/domain/user/util/UserInfo.java
@@ -1,5 +1,7 @@
 package com.sideprj.ipoAlarm.domain.user.util;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +9,7 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Parameter(hidden = true)
 public @interface UserInfo {
 
 
